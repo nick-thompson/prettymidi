05bacba4f1aa670d553729bbbabf10362420769e
Nick Thompson <nct25@cornell.edu>
2012-11-26 15:41:47 -0500

    Redo of json output

--- test/step.js
+++ test/step.js
@@ -3,7 +3,8 @@ var exec    = require("child_process").exec
   , fs      = require("fs")
   , chai    = require("chai")
   , expect  = chai.expect
-  , should  = chai.should();
+  , should  = chai.should()
+  , exists  = fs.existsSync;
 
 describe("step default", function () {
 
@@ -12,7 +13,23 @@ describe("step default", function () {
   });
 
   it("Decodes a midi file", function (done) {
-    exec("bin/step test/fixtures/example-yctwykm.mid", function (err, stdout) {
+    exec("bin/step test/fixtures/example-ctwykm.mid", function (err, stdout) {
+      if (err) { done(err); }
+      stdout.should.include("written");
+      expect(exists("stepfiles/example-ctwykm.js")).to.equal(true);
+      done();
+    });
+  });
+
+  it("Can print plain json", function (done) {
+    exec("bin/step -j test/fixtures/example-ctwykm.mid", function (err, stdout) {
+      if (err) { done(err); }
+      stdout.should.include("written");
+      expect(exists("stepfiles/example-ctwykm.json")).to.equal(true);
+
+      var data = JSON.parse(fs.readFileSync("stepfiles/example-ctwykm.json"));
+      expect(data).to.be.a("object");
+      data.should.have.property("nTracks");
       done();
     });
   });
