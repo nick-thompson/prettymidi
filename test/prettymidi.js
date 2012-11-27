d30605a7c0d2ccdcb2aa2dc0e32a8e15c451a93d
Nick Thompson <nct25@cornell.edu>
2012-11-26 23:47:44 -0500

    Lint free and outputting straight json

--- test/prettymidi.js
+++ test/prettymidi.js
@@ -8,43 +8,26 @@ var exec    = require("child_process").exec
 
 describe("prettymidi default", function () {
 
-  afterEach(function (done) {
-    exec("rm -rf prettymidifiles", done);
-  });
-
-  it("Decodes a midi file", function (done) {
-    exec("bin/prettymidi test/fixtures/example-ctwykm.mid", function (err, stdout) {
-      if (err) { done(err); }
-      stdout.should.include("written");
-      expect(exists("prettymidifiles/example-ctwykm.js")).to.equal(true);
-      done();
-    });
-  });
-
-  it("Can print plain json", function (done) {
-    exec("bin/prettymidi -j test/fixtures/example-ctwykm.mid", function (err, stdout) {
-      if (err) { done(err); }
-      stdout.should.include("written");
-      expect(exists("prettymidifiles/example-ctwykm.json")).to.equal(true);
-
-      var data = JSON.parse(fs.readFileSync("prettymidifiles/example-ctwykm.json"));
-      expect(data).to.be.a("object");
-      data.should.have.property("ticksPerBeat");
-      done();
-    });
-  });
-
   describe("Decoding a single file", function () {
 
     it("outputs a json file in the same directory", function (done) {
-      done();
+      exec("bin/prettymidi test/fixtures/example-ctwykm.mid", function (err, stdout) {
+        if (err) { done(err); }
+        stdout.should.include("written");
+        expect(exists("example-ctwykm.json")).to.equal(true);
+
+        var data = JSON.parse(fs.readFileSync("example-ctwykm.json"));
+        expect(data).to.be.a("object");
+        data.should.have.property("ticksPerBeat");
+
+        exec("rm example-ctwykm.json", function (err, stdout) {
+          if (err) { done(err); }
+          done();
+        });
+      });
     });
 
     it("accepts an output destination flag", function (done) {
-      done();
-    });
-
-    it("accepts an amd wrapper flag", function (done) {
       done();
     });
   });
