b45523369221ac761d189c3cbc9757dd6122e908
Nick Thompson <ncthom91@gmail.com>
2013-01-20 22:52:39 -0500

    Reestablish test file.

---
+++ test/test.js
@@ -0,0 +1,21 @@
+var pm      = require("../index")
+  , chai    = require("chai")
+  , expect  = chai.expect
+  , should  = chai.should()
+  , fs      = require("fs")
+  , path    = require("path");
+
+describe("prettymidi", function () {
+
+  var fp = path.resolve(__dirname, "fixtures/break.mid")
+    , buffer = fs.readFileSync(fp);
+
+  it("Correctly decodes a MIDI file buffer.", function () {
+
+    var data = pm.decode(buffer);
+    expect(data).to.be.a("object");
+    expect(data.format).to.be.equal(1);
+
+  });
+
+});
