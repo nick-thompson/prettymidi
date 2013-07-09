14ab0421f5931e88c6e7fcdebea901823885f8bc
Nick Thompson <ncthom91@gmail.com>
2013-07-09 01:26:44 -0400

    Include a function for pretty printing device list

--- index.js
+++ index.js
@@ -41,6 +41,30 @@ var map =  {
 };
 
 /**
+ * Return valid devices from the Web MIDI API.
+ *
+ * @param {object} access MidiAccess object.
+ * @api public
+ */
+
+module.exports.readDevices = function (access) {
+  var inputs = access.getInputs()
+    , outputs = access.getOutputs()
+    , devices = {};
+
+  devices.inputs = inputs.map(function (input, index) {
+    input.port = index;
+    return input;
+  });
+
+  devices.outputs = outputs.map(function (output, index) {
+    output.port = index;
+    return output;
+  });
+
+};
+
+/**
  * Decoder constructor.
  *
  * @api private
